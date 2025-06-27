49499dfc8f06a8127539533d47fd0edea6a0e88e
shou-317 <shou6130@gmail.com>
2025-06-27 18:37:50 +0900

    refactor: wrap DB access methods with with_connection for cleaner resource management

--- model.rb
+++ model.rb
@@ -2,45 +2,55 @@
 
 require 'pg'
 
-def read_all
+def with_connection
   connection = PG.connect(dbname: 'memo-app')
-  result = connection.exec('SELECT * FROM memos;')
-  result.map { |row| row.transform_keys(&:to_sym) }
-  ensure connection.close
+  yield(connection)
+ensure
+  connection.close
+end
+
+def read_all
+  with_connection do |connection|
+    result = connection.exec('SELECT * FROM memos;')
+    result.map { it.transform_keys(&:to_sym) }
+  end
 end
 
 def read(id)
-  connection = PG.connect(dbname: 'memo-app')
-  result = connection.exec_params('SELECT * FROM memos WHERE id = $1;', [id])
-  result.first.transform_keys(&:to_sym)
-  ensure connection.close
+  with_connection do |connection|
+    result = connection.exec_params(
+      'SELECT * FROM memos WHERE id = $1;',
+      [id]
+    )
+    result.first.transform_keys(&:to_sym)
+  end
 end
 
 def create(params)
-  connection = PG.connect(dbname: 'memo-app')
-  result = connection.exec_params(
-    'INSERT INTO memos (title, content) VALUES ($1, $2) RETURNING *',
-    [params[:title], params[:content]]
-  )
-  result.first.transform_keys(&:to_sym)
-  ensure connection.close
+  with_connection do |connection|
+    result = connection.exec_params(
+      'INSERT INTO memos (title, content) VALUES ($1, $2) RETURNING *',
+      [params[:title], params[:content]]
+    )
+    result.first.transform_keys(&:to_sym)
+  end
 end
 
 def update(params)
-  connection = PG.connect(dbname: 'memo-app')
-  result = connection.exec_params(
-    'UPDATE memos
-    SET title = $1, content = $2
-    WHERE id = $3
-    RETURNING *;',
-    [params[:title], params[:content], params[:id]]
-  )
-  result.first.transform_keys(&:to_sym)
-  ensure connection.close
+  with_connection do |connection|
+    result = connection.exec_params(
+      'UPDATE memos SET title = $1, content = $2 WHERE id = $3 RETURNING *;',
+      [params[:title], params[:content], params[:id]]
+    )
+    result.first.transform_keys(&:to_sym)
+  end
 end
 
 def delete(id)
-  connection = PG.connect(dbname: 'memo-app')
-  connection.exec_params('DELETE FROM memos WHERE id = $1;', [id])
-  ensure connection.close
+  with_connection do |connection|
+    result = connection.exec_params(
+      'DELETE FROM memos WHERE id = $1;',
+      [id]
+    )
+  end
 end
